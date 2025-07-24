879e3edbfced63314156057d0c62b6308fe793ba
yaesolKim <kimyaesol@gmail.com>
2025-07-24 14:40:08 +0900

    step2

--- Project7/testable_sms_sender.cpp
+++ Project7/testable_sms_sender.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
+#include "gmock/gmock.h"
 #include "sms_sender.cpp"
+
 class TestableSmsSender : public SmsSender {
 public:
-	void send(Schedule* schedule) override {
-		std::cout << "테스트용 SmsSender class의 send메서드 실행됨\n";
-		sendMethodIsCalled = true;
-	}
-	bool isSendMethodIsCalled() {
-		return sendMethodIsCalled;
-	}
-private:
-	bool sendMethodIsCalled;
+	MOCK_METHOD(void, send, (Schedule*), (override));
 };
